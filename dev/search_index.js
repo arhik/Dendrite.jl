692d80c1d79d21379965b5b9b54a79caff63cca7
Documenter.jl <documenter@juliadocs.github.io>
2022-10-13 18:38:36 +0000

    build based on fa0a3d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Dendrite","category":"page"},{"location":"#Dendrite","page":"Home","title":"Dendrite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Dendrite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Dendrite]","category":"page"}]
+}
